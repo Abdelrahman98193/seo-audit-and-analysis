ba7995c61c00e25028c991ab480f605079d9d7bb
Abdelrahman Saber <69966590+Abdelrahman98193@users.noreply.github.com>
2024-02-12 02:08:08 +0200

    Update crawlytics.py

--- crawlytics.py
+++ crawlytics.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pyarrow.parquet as pq
-
-import re
 import advertools as adv
 
 # UI
@@ -14,15 +11,15 @@ if __name__ == '__main__':
     st.sidebar.title('SEO Analysis App')
     st.sidebar.write('You can analyze SEO data with this app.')
 
-    uploaded_file = st.file_uploader("Upload your data file (.jl)", type='jl')
+    uploaded_file = st.file_uploader("Upload your data file (.csv)", type='csv')
     if uploaded_file is not None:
         st.write('File uploaded successfully!')
         columns = st.text_input('Enter columns to analyze (separate by comma)', 'url, status, redirect_urls, redirect_reasons')
 
         # Process the uploaded file
         try:
-            crawldf = pd.read_json(uploaded_file, lines=True)
-            final_df = redirect_summary(crawldf)
+            crawldf = pd.read_csv(uploaded_file)
+            final_df = redirect_summary(crawldf)  # Assuming `redirect_summary` is the function you want to use
             st.write(final_df)
         except Exception as e:
             st.write('Error processing the file. Please check the data format.')
